3d80f82bb209846c7643b2588d27577b1f235aaf
Denis <den.sautner@mail.ru>
2024-03-28 17:05:38 +0600

    feat: layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
+import * as styles from './AppStyle'
 import { GlobalStyle } from './GlobalStyle'
 
-export const App = () => (
-  <>
-    <GlobalStyle />
-    <div>It works</div>
-  </>
-)
+export const App = () => {
+  return (
+    <>
+      <GlobalStyle />
+      <styles.Wrapper>
+        <styles.SearchContainer>
+          <styles.Search placeholder='Введите логин' />
+          <styles.SortContainer>
+            <styles.SortText>Репозитории по:</styles.SortText>
+            <styles.SortSelect>
+              <styles.SortSelectOption>Возрастанию</styles.SortSelectOption>
+              <styles.SortSelectOption>Убыванию</styles.SortSelectOption>
+            </styles.SortSelect>
+          </styles.SortContainer>
+        </styles.SearchContainer>
+        <styles.UsersContainer></styles.UsersContainer>
+      </styles.Wrapper>
+    </>
+  )
+}
--- src/GlobalStyle.tsx
+++ src/GlobalStyle.tsx
@@ -1,12 +1,18 @@
 import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle`
-    * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        font-family: 'Montserrat-Regular';
-    }
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Montserrat-Regular';
+}
+
+body {
+    background-color: #6600FF;
+    display: flex;
+    justify-content: center;
+}
 
 @font-face {
     font-family: 'Montserrat-Regular';
